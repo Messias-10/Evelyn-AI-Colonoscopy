8b620881da2de1926bf6817fce047fdd6cc3a282
Messias <luckasreyes945@gmail.com>
2025-02-04 17:28:41 -0300

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectodeTituloFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded; // Inicia el video cuando la ventana se cargue
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            VideoPlayer.Play(); // Inicia la reproducción manualmente
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            VideoPlayer.Position = TimeSpan.Zero; // Reinicia el video al inicio
+            VideoPlayer.Play(); // Vuelve a reproducirlo
+        }
+    }
+}
